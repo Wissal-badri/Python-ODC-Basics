0ee73c1750e75a061b87dbc39e0478feb17cc91a
Wissal-badri <badriwiwi1@gmail.com>
2026-02-04 11:51:32 +0100

    Fisrt commit

---
+++ script.py
@@ -0,0 +1,62 @@
+#Afficchage un message simple
+print("Hello, ODC!")
+
+#Variables 
+
+#camelCase notation
+#nomEtudiant= "Wissal"
+#snake_case notation
+#nom_etudiant= "Wissal"
+#Kebab-case notation
+#nom-etudiant= "Wissal"
+#Pas de notation
+#nometudiant= "Wissal"
+
+#Exemple 
+# Prix_casquette= 20
+# print("Le prix de la casquette est de", Prix_casquette, "Dirham")
+
+#input function
+# nom= input("Entrez votre nom: ")
+# age= input("Entrez votre age: ")
+# #affichage des variables
+# print("Votre nom est:", nom)
+# print("Votre age est:", age)
+
+# #Exercice
+# A= "Wissal"
+# B= "Badri"
+# print ("Avant l'échange: A =", A, ", B =", B)
+# #Echange des valeurs
+# temp= A
+# A= B
+# B= temp
+# print ("Après l'échange: A =", A, ", B =", B)
+
+#Les oprateurs arithmétiques
+# N=14
+# X=20
+# print ("La somme de N et X est:", N + X)
+# print ("La différence de N et X est:", X - N)
+# print ("Le produit de N et X est:", N * X)
+# print ("Le quotient de X par N est:", X / N)
+# print ("Le reste de la division de X par N est:", X % N)
+# print ("La puissance de N à la X est:", N ** X)
+# print ("La division entière de X par N est:", X // N)
+
+# #Exercice
+# # Demander à l'utilisateur d'entrer deux nombres
+# nombre1 = int(input("Entrer le premier nombre : "))
+# nombre2 = int(input("Entrer le deuxième nombre : "))
+
+# # Calcul de la somme
+# somme = nombre1 + nombre2
+
+# # Affichage du résultat
+# print("La somme est :", somme)
+
+#La concatenation des chaines de caractères String
+# prenom= input("Entrez votre prénom: ")
+# nom= input("Entrez votre nom: ") 
+# print("Bonjour, " + prenom + " " + nom + "!")
+
