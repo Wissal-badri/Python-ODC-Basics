cdd0bbca568cb92731ff32f28318efdf9e937120
Wissal-badri <badriwiwi1@gmail.com>
2026-02-04 13:43:57 +0100

    Implement cinema ticket pricing system and comment out previous student grade evaluation logic

--- script.py
+++ script.py
@@ -177,17 +177,44 @@
 # else:
 #     print("Nul")
 
-#Exercice
-note = float(input("Entrez la note de l'étudiant : "))
-if note > 20 or note < 0:
-    print("Note invalide")
-elif note >= 16:
-    print("La mention est : Très bien")
-elif note >= 14:
-    print("La mention est : Bien")
-elif note >= 12:
-    print("La mention est : Assez bien")
-elif note >= 10:
-    print("La mention est : Passable")
-else:
-    print("La mention est : Insuffisant")
+# #Exercice
+# note = float(input("Entrez la note de l'étudiant : "))
+# if note > 20 or note < 0:
+#     print("Note invalide")
+# elif note >= 16:
+#     print("La mention est : Très bien")
+# elif note >= 14:
+#     print("La mention est : Bien")
+# elif note >= 12:
+#     print("La mention est : Assez bien")
+# elif note >= 10:
+#     print("La mention est : Passable")
+# else:
+#     print("La mention est : Insuffisant")
+
+#Exercice : Système de gestion des tickets de cinéma
+
+print("--- Système de gestion des tickets de cinéma ---")
+# 1. Demander l'âge
+age = int(input("Veuillez entrer votre âge : "))
+# 2. Demander la carte d'étudiant
+carte = input("Avez-vous une carte d'étudiant ? (oui/non) : ")
+# 3. Demander l'horaire
+horaire = input("Quelle est l'horaire de la séance ? (matin, après-midi ou soir) : ")
+prix = 0
+if age < 12:
+    prix = 50
+elif 12 <= age <= 17:
+    if carte == "oui":
+        prix = 50
+    else:
+        prix = 70
+else:  # age >= 18
+    if horaire == "soir":
+        prix = 120
+    else:
+        prix = 100
+    # Appliquer la réduction étudiant
+    if carte == "oui":
+        prix -= 20
+print("Le prix du billet est de :", prix, "DH")
