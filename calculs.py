ca9194a8917ddd56738f93aafb14e5578de44389
Wissal-badri <badriwiwi1@gmail.com>
2026-02-05 14:24:13 +0100

    feat: Add `calculs.py` demonstrating various `math` module functions.

---
+++ calculs.py
@@ -0,0 +1,36 @@
+# import calculs
+
+
+# print("Résultats des opérations de calcul :")
+
+# print(calculs.addition(10, 20))
+# print(calculs.soustraction(10, 20))
+# print(calculs.multiplication(10, 20))
+# print(calculs.division(10, 20))
+
+import math
+# Exemple d'utilisation de la fonction math.sqrt() pour calculer la racine carrée
+nombre = 16
+racine_carre = math.sqrt(nombre)
+print(f"La racine carrée de {nombre} est {racine_carre}")
+
+# Exemple d'utilisation de la fonction math.pow() pour calculer la puissance
+base = 2
+exposant = 3
+resultat = math.pow(base, exposant)
+print(f"{base} élevé à la puissance {exposant} est {resultat}")
+
+# Exemple d'utilisation de la fonction math.factorial() pour calculer la factorielle
+nombre = 5
+factorielle = math.factorial(nombre)
+print(f"La factorielle de {nombre} est {factorielle}")
+
+# Exemple d'utilisation de la fonction math.sin() pour calculer le sinus d'un angle en radians
+angle = math.pi / 2
+sinus = math.sin(angle)
+print(f"Le sinus de {angle} est {sinus}")
+
+# Exemple d'utilisation de la fonction math.log() pour calculer le logarithme d'un nombre
+nombre = 10
+logarithme = math.log(nombre)
+print(f"Le logarithme de {nombre} est {logarithme}")
